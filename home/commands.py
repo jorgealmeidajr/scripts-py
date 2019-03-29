c9d269505fa93e565e4c464ae969a43cfaa94ce2
Jorge Almeida Junior <jorgealmeidajunior@gmail.com>
2019-03-29 17:35:57 -0300

    Add mv_tutorials_to_dropbox command

--- home/commands.py
+++ home/commands.py
@@ -1,7 +1,17 @@
+import os
+
 import home.constants as constants
 
 
-def print_constants():
-    print(constants.HOME_DIRECTORY)
-    print(constants.DOWNLOADS_DIRECTORY)
-    print(constants.DROPBOX_DIRECTORY)
+def print_home_commands():
+    pass
+
+
+def mv_tutorials_to_dropbox():
+    tutorials_path = os.path.join(constants.DOWNLOADS_DIRECTORY, 'tutorials')
+    files = []
+
+    with os.scandir(tutorials_path) as entries:
+        for entry in entries:
+            if entry.is_file():
+                files.append(entry)
--- scripts.py
+++ scripts.py
@@ -3,8 +3,9 @@ import home.commands as home_commands
 
 def main():
     print("scripts.py called")
-    home_commands.print_constants()
+    # home_commands.print_home_commands()
+    home_commands.mv_tutorials_to_dropbox()
 
 
-if __name__== "__main__":
+if __name__ == "__main__":
     main()
