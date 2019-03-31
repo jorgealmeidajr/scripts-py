3561d650d7e3681e931ea2c77c73372a9deb6de1
Jorge Almeida Junior <jorgealmeidajunior@gmail.com>
2019-03-31 19:14:10 -0300

    Command mv_tutorials_to_dropbox implemented

--- home/commands.py
+++ home/commands.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 
 import home.constants as constants
 
@@ -9,9 +10,29 @@ def print_home_commands():
 
 def mv_tutorials_to_dropbox():
     tutorials_path = os.path.join(constants.DOWNLOADS_DIRECTORY, 'tutorials')
+    extensions = ['.pdf']
     files = []
 
     with os.scandir(tutorials_path) as entries:
         for entry in entries:
             if entry.is_file():
-                files.append(entry)
+                name, ext = os.path.splitext(entry.path)
+                if ext in extensions:
+                    files.append({
+                        "path": entry.path,
+                        "name": entry.name
+                    })
+
+    if len(files) > 0:
+        print('There are files to be moved from Downloads to Dropbox')
+        print('Moving files...')
+
+        destination = os.path.join(constants.DROPBOX_DIRECTORY, 'tutorials')
+
+        for file in files:
+            shutil.move(file['path'], destination)
+
+        print('Files moved with success!')
+
+    else:
+        print('There are not files to be moved')
--- scripts.py
+++ scripts.py
@@ -1,10 +1,19 @@
+import argparse
+
 import home.commands as home_commands
 
 
 def main():
-    print("scripts.py called")
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-c", "--command", required=True,
+                    help="Command to execute")
+    args = vars(ap.parse_args())
+
+    print(f"Command to execute: {args['command']}")
+
     # home_commands.print_home_commands()
-    home_commands.mv_tutorials_to_dropbox()
+    if args['command'] == 'mv-tutorials-to-dropbox':
+        home_commands.mv_tutorials_to_dropbox()
 
 
 if __name__ == "__main__":
